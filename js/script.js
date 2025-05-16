d5be5063b569e91fb544947df713bc209159f459
Githubshakil <shakilmahmud007@gmail.com>
2025-05-16 23:20:32 +0600

    update

--- js/script.js
+++ js/script.js
@@ -16,9 +16,14 @@ const HexValues = () =>{
 const handleButton1= () => {
    let rgb1= HexValues()
    console.log(rgb1);
+}
+
+const handleButton2 = () => {
+   let rgb2= HexValues()
+   console.log(rgb2);
+}
    
         
-}
 
 btn1.addEventListener("click", handleButton1)
 btn2.addEventListener("click", handleButton2)
