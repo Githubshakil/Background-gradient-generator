f787c3b18751316051028edac1944c565da8c831
Githubshakil <shakilmahmud007@gmail.com>
2025-05-16 20:46:43 +0600

    update

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let btn1 = document.getElementById('myButton')
+let btn2 = document.getElementById('myButton2')
+
+const HexValues = () =>{
+     let myHexValues = "123456789abcdef"; 
+    let colors = "#";
+    for(i=0; i < 6; i++ ){
+        colors= colors + (myHexValues[Math.floor(Math.random() * 16)]);
+        
+    }
+        return colors ;
+
+}
+
+
+const handleButton1= () => {
+   let rgb1= HexValues()
+   console.log(rgb1);
+   
+        
+}
+
+btn1.addEventListener("click", handleButton1)
+btn2.addEventListener("click", handleButton2)
