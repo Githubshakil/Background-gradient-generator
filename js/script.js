3a3d1971d778b8e41d811118f62853186ab6339c
Githubshakil <shakilmahmud007@gmail.com>
2025-05-16 23:43:20 +0600

    update

--- js/script.js
+++ js/script.js
@@ -1,26 +1,30 @@
 let btn1 = document.getElementById('myButton')
 let btn2 = document.getElementById('myButton2')
+let copyDiv = document.querySelector(".copyCode")
 
-const HexValues = () =>{
-     let myHexValues = "123456789abcdef"; 
+const HexValues = () => {
+    let myHexValues = "0123456789abcdef"; 
     let colors = "#";
-    for(i=0; i < 6; i++ ){
-        colors= colors + (myHexValues[Math.floor(Math.random() * 16)]);
-        
+    for (let i = 0; i < 6; i++) {
+        colors = colors + (myHexValues[Math.floor(Math.random() * 16)]);
     }
-        return colors ;
-
+    return colors;
 }
 
 
 const handleButton1= () => {
    let rgb1= HexValues()
    console.log(rgb1);
+   document.body.style.backgroundImage = `linear-gradient(to right, ${rgb1}, #444)`
+   copyDiv.innerHTML = `background-image: linear-gradient(to right, ${rgb1}, rgb(152, 63, 198))`
 }
 
 const handleButton2 = () => {
    let rgb2= HexValues()
    console.log(rgb2);
+   document.body.style.backgroundImage = `linear-gradient(to right, #000, ${rgb2})`
+   copyDiv.innerHTML = `background-image: linear-gradient(to right, #000, ${rgb2})`
+
 }
    
         
